8d259e368eefb8aa61631a7ed1643db855ac25d2
tatthang0501 <tatthang0501@gmail.com>
2021-04-08 21:35:01 +0700

    minimize output

--- src/main/java/ptit/web/LogOut.java
+++ src/main/java/ptit/web/LogOut.java
@@ -1,7 +1,5 @@
 package ptit.web;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
