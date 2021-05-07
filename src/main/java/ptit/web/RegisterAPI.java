169e10364c8f37db2434e80ff4c72359fd0c1d31
tatthang0501 <57296747+tatthang0501@users.noreply.github.com>
2021-05-07 11:13:38 +0700

    up

--- src/main/java/ptit/web/RegisterAPI.java
+++ src/main/java/ptit/web/RegisterAPI.java
@@ -1,26 +1,22 @@
 package ptit.web;
 
 import java.io.IOException;
-import java.security.Principal;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.RollbackException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.hibernate.SessionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -39,7 +35,6 @@ import ptit.KipHoc;
 import ptit.KyHoc;
 import ptit.LichHoc;
 import ptit.LichHocView;
-import ptit.ListDaDK_ListCoTheDK;
 import ptit.LopHocPhan;
 import ptit.MonHoc;
 import ptit.MonHocKyHoc;
@@ -50,14 +45,11 @@ import ptit.TuanHoc;
 import ptit.common.JwtUtils;
 import ptit.data.BoMonRepository;
 import ptit.data.GiangVienKhoaRepository;
-import ptit.data.KipHocRepository;
 import ptit.data.KyHocRepository;
 import ptit.data.LichHocRepository;
 import ptit.data.LopHocPhanRepository;
 import ptit.data.MonHocKyHocRepository;
 import ptit.data.MonHocRepository;
-import ptit.data.NgayHocRepository;
-import ptit.data.TuanHocRepository;
 import ptit.data.UserRepository;
 import ptit.dto.JwtResponse;
 import ptit.dto.LoginForm;
@@ -79,12 +71,7 @@ public class RegisterAPI {
     private final LopHocPhanRepository lhpRepo;
     @Autowired
     private final LichHocRepository lhRepo;
-    @Autowired
-    private final KipHocRepository khRepo;
-    @Autowired
-    private final NgayHocRepository nhRepo;
-    @Autowired
-    private final TuanHocRepository thRepo;
+
     @Autowired
     private final GiangVienKhoaRepository gvkRepo;
     @Autowired
@@ -103,16 +90,12 @@ public class RegisterAPI {
     JwtUtils jwtUtils;
 
     public RegisterAPI(KyHocRepository kyhocRepo, MonHocKyHocRepository mhkhRepo, MonHocRepository mhRepo,
-            LopHocPhanRepository lhpRepo, LichHocRepository lhRepo, KipHocRepository khRepo, NgayHocRepository nhRepo,
-            TuanHocRepository thRepo, GiangVienKhoaRepository gvkRepo, BoMonRepository bmRepo) {
+            LopHocPhanRepository lhpRepo, LichHocRepository lhRepo, GiangVienKhoaRepository gvkRepo, BoMonRepository bmRepo) {
         this.kyhocRepo = kyhocRepo;
         this.mhkhRepo = mhkhRepo;
         this.mhRepo = mhRepo;
         this.lhpRepo = lhpRepo;
         this.lhRepo = lhRepo;
-        this.khRepo = khRepo;
-        this.nhRepo = nhRepo;
-        this.thRepo = thRepo;
         this.gvkRepo = gvkRepo;
         this.bmRepo = bmRepo;
     }
@@ -186,20 +169,20 @@ public class RegisterAPI {
                 bm.setDsMonHoc(listMH);
             }
             System.out.print("/n");
-            for(int i : listIdMon){
+            for (int i : listIdMon) {
                 System.out.println(i);
             }
             ArrayList<KyHoc> listKy = (ArrayList<KyHoc>) kyhocRepo.findAll();
             KyHoc newestKH = listKy.get(listKy.size() - 1);
             ArrayList<MonHocKyHoc> listMHKHTemp = (ArrayList<MonHocKyHoc>) mhkhRepo.getListMHKH(newestKH.getId());
             ArrayList<MonHocKyHoc> listMHKH = new ArrayList<MonHocKyHoc>();
-            
+
             for (MonHocKyHoc mhkh : listMHKHTemp) {
                 if (listIdMon.contains(mhkh.getMh().getId())) {
                     MonHoc mh = mhRepo.findById(mhkh.getMh().getId()).get();
                     mhkh.setMh(mh);
                     listMHKH.add(mhkh);
-                } 
+                }
             }
 
             ArrayList<MonHocKyHocView> listMHKHView = new ArrayList<MonHocKyHocView>();
@@ -235,26 +218,24 @@ public class RegisterAPI {
                 LichHoc lh = listLichHoc.get(0);
                 lh.setLhp(lhp);
                 List<KipHoc> kh = lh.getKipHoc();
-                for(KipHoc kip : kh){
+                for (KipHoc kip : kh) {
                     kip.setLh(null);
                 }
                 lh.setKipHoc(kh);
 
                 List<NgayHoc> nh = lh.getNgayHoc();
-                for(NgayHoc ngay : nh){
+                for (NgayHoc ngay : nh) {
                     ngay.setLh(null);
                 }
                 lh.setNgayHoc(nh);
 
                 List<TuanHoc> th = lh.getTuanHoc();
-                for(TuanHoc tuan : th){
+                for (TuanHoc tuan : th) {
                     tuan.setLh(null);
                 }
                 lh.setTuanHoc(th);
                 listLichLHP.add(lh);
             }
-
-            ArrayList<LichHoc> listLichDaDK = (ArrayList<LichHoc>) lhRepo.findDaDKLHP(tv.getId());
 
             // ArrayList<LichHocView> listLichViewDaDK = new ArrayList<LichHocView>();
             ArrayList<LichHocView> listLichViewLHP = new ArrayList<LichHocView>();
@@ -266,50 +247,35 @@ public class RegisterAPI {
                 lhv.setPhong(lh.getPhong());
                 lhv.setNhomTH(lh.getNhomTH());
                 lhv.setSiSoToiDa(lh.getLhp().getSisotoida());
-                
+
                 List<KipHoc> kh = lh.getKipHoc();
                 List<Integer> listKH = new ArrayList<>();
-                for(KipHoc kip : kh){
+                for (KipHoc kip : kh) {
                     listKH.add(kip.getTen());
                 }
                 lhv.setKipHoc(listKH);
 
                 List<NgayHoc> nh = lh.getNgayHoc();
                 List<Integer> listNH = new ArrayList<>();
-                for(NgayHoc ngay : nh){
+                for (NgayHoc ngay : nh) {
                     listNH.add(ngay.getTen());
                 }
                 lhv.setNgayHoc(listNH);
 
                 List<TuanHoc> th = lh.getTuanHoc();
                 List<Integer> listTH = new ArrayList<>();
-                for(TuanHoc tuan : th){
+                for (TuanHoc tuan : th) {
                     listTH.add(tuan.getTen());
                 }
                 lhv.setTuanHoc(listTH);
 
                 listLichViewLHP.add(lhv);
-                if(lh.getGv() != null){
+                if (lh.getGv() != null) {
                     lhv.setDaDK(true);
                 }
             }
-            // for (LichHoc lh : listLichDaDK) {
-            //     LichHocView lhv = new LichHocView();
-            //     lhv.setId(lh.getId());
-            //     lhv.setTen(lh.getTen());
-            //     lhv.setSoTC(lh.getLhp().getMhkh().getMh().getSoTC());
-            //     lhv.setPhong(lh.getPhong());
-            //     lhv.setNhomTH(lh.getNhomTH());
-            //     lhv.setSiSoToiDa(lh.getLhp().getSisotoida());
-            //     listLichViewDaDK.add(lhv);
-            // }
-            // ListDaDK_ListCoTheDK list = new ListDaDK_ListCoTheDK();
-            // list.setListLichDaDK(listLichViewDaDK);
-            // list.setListLichLHP(listLichViewLHP);
-            // session.setAttribute("listDaDK", listLichDaDK);
             return new ResponseEntity<>(listLichViewLHP, HttpStatus.OK);
-        } 
-        catch (Exception e) {
+        } catch (Exception e) {
             model.addAttribute("msg", "Có lỗi xảy ra khi lấy danh sách lớp học phần");
             return new ResponseEntity<>("fail", HttpStatus.NOT_FOUND);
         }
@@ -321,26 +287,31 @@ public class RegisterAPI {
         try {
             System.out.println("Bat dau update dang ky ");
             System.out.println(listDK.size());
-            System.out.println(listDK.get(0).getTen()+" id day"+ listDK.get(0).getId());
+            System.out.println(listDK.get(0).getTen() + " id day" + listDK.get(0).getId());
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             String currentPrincipalName = authentication.getName();
             ThanhVien tv = userRepository.findByUsername(currentPrincipalName).get();
             System.out.println("Id thang user day " + tv.getId() + tv.getTen());
             for (LichHocView lh : listDK) {
                 System.out.println("tao dang chay day");
-                int count = lhRepo.updateDangKy(tv.getId(), lh.getId());
-                if(count != 1){
-                    return new ResponseEntity<>("Có lỗi trong quá trình update",HttpStatus.NOT_MODIFIED);
+                if (lh.isDaDK() == false) {
+                    int count = lhRepo.updateDangKy(tv.getId(), lh.getId());
+                    if (count != 1) {
+                        return new ResponseEntity<>("Có lỗi hệ thống trong quá trình update", HttpStatus.NOT_MODIFIED);
+                    }
+                }
+                else {
+                    return new ResponseEntity<>("Lớp đã có người đăng kí, phát hiện gian lận", HttpStatus.NOT_MODIFIED);
                 }
             }
             System.out.println("Luu dang ky thah cong");
             // model.addAttribute("msg", msg);
-            return new ResponseEntity<>("update successful", HttpStatus.OK);
+            return new ResponseEntity<>("Cập nhật danh sách lớp học phần thành công", HttpStatus.OK);
         }
-         catch (Exception e) {
-            String msg = "Có lỗi xảy ra khi lưu danh sách đăng ký";
+        catch (Exception e) {
+            // String msg = "Có lỗi xảy ra khi lưu danh sách đăng ký";
             // model.addAttribute("msg", msg);
-            return new ResponseEntity<>("fail", HttpStatus.NOT_MODIFIED);
+            return new ResponseEntity<>("Có lỗi xảy ra trong quá trình update", HttpStatus.NOT_MODIFIED);
         }
     }
 }
